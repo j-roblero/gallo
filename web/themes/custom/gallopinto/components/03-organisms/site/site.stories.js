e260c1915bd9c58c9a820141a3ad3c31ebbcaa30
Jessy Roblero Obando <j.roblero@paralleldevs.com>
2023-09-29 00:04:43 -0600

    update: add footer

---
+++ web/themes/custom/gallopinto/components/03-organisms/site/site.stories.js
@@ -0,0 +1,8 @@
+import siteFooter from './site-footer/site-footer.twig';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Organisms/Site' };
+
+export const header = () => siteFooter({});
