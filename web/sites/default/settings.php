831dc0a8389c5867efa9043a1f46cca4cff998e8
Jessy Roblero Obando <j.roblero@paralleldevs.com>
2023-09-29 14:52:21 -0600

    master: line break removed

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -37,7 +37,6 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-
 /**
  * Add the following lines to the database config array.
  *
