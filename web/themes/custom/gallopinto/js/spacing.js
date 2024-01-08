26ff3c74750e64ac996b2378fd8116307be48fa1
Sofía <s.sanchez@paralleldevs.com>
2024-01-08 13:36:36 -0600

    gp-update:Paragraph spacing, fields on paragraph section, js spacing

---
+++ web/themes/custom/gallopinto/js/spacing.js
@@ -0,0 +1,21 @@
+(function ($) {
+  Drupal.behaviors.updateSpacing = {
+    attach: function (context, settings) {
+
+      console.log('entre');
+      
+      let sizes = ['lg', 'md', 'sm'];
+
+      $('.field--name-field-value-lg').css('display', 'none');
+      $('.field--name-field-value-md').css('display', 'none');
+      $('.field--name-field-value-sm').css('display', 'none');
+
+      sizes.forEach(function (size) {
+        ['top', 'right', 'bottom', 'left'].forEach(function (direction) {
+          var $select = $('[data-drupal-selector="edit-field-spacings-0-subform-field-' + direction + '-' + size + '"]');
+          $select.find('option[value="_none"]').remove();
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
